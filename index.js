78799247aee1d6996f4aa78aee2320abd740ac1a
shakeel ayaan <100689042+ShakeelAyaan@users.noreply.github.com>
2023-06-06 20:36:24 +0530

    changed url

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const emailForm = document.querySelector("#emailForm");
 const toast = document.querySelector(".toast");
 
 // const baseURL = "https://innshare.herokuapp.com";
-const baseURL ="http://localhost:3000"
-// const baseURL = "https://data-sharing-bice.vercel.app/"
+// const baseURL ="http://localhost:3000"
+const baseURL = "https://node-sharing.vercel.app/"
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
