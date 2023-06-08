ff340d62dd938167dc7050a1e1abf64796110bb5
shakeel ayaan <100689042+ShakeelAyaan@users.noreply.github.com>
2023-06-08 14:36:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const toast = document.querySelector(".toast");
 // const baseURL = "https://innshare.herokuapp.com";
 // const baseURL ="http://localhost:3000"
 // const baseURL = "https://node-sharing-7gsa.vercel.app"
-   const baseURL = "https://node-sharing-7gsa-a5jcms7vq-shakeelayaan.vercel.app"
+   const baseURL = "https://node-sharing.vercel.app"
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
